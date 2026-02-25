66fa402c62b9e18249c8845ae0ad68cb6fc472e6
valentin richard <valentinrich@gmail.com>
2026-02-25 14:06:35 +0100

    Update packages/tokens/scripts/shared/dtcg/design-token/token/design-token.ts
    Co-authored-by: Victor Jolissaint <victor.jolissaint@infomaniak.com>

--- packages/tokens/scripts/shared/dtcg/design-token/token/design-token.ts
+++ packages/tokens/scripts/shared/dtcg/design-token/token/design-token.ts
@@ -6,14 +6,12 @@ import type { ValueOrDesignTokenReference } from '../reference/value-or/value-or
  * @inheritDoc https://www.designtokens.org/tr/2025.10/format/#design-token-0
  *
  * > An object with a $value property is a token
- */
-export interface DesignToken<GType extends string, GValue> {
-  /**
-   * @alias $ref
-   */
-  readonly $value: ValueOrDesignTokenReference<GValue>;
-  readonly $type?: GType;
-  readonly $deprecated?: boolean | string;
-  readonly $description?: string;
-  readonly $extensions?: Record<string, ExplicitAny>;
+import type { ValueOrDesignTokenReference } from '../reference/value-or/value-or-design-token-reference.ts';
+
+/**
+@@ -14,5 +15,5 @@ export interface DesignToken<GType extends string, GValue> {
+readonly $type?: GType;
+readonly $deprecated?: boolean | string;
+readonly $description?: string;
+readonly $extensions?: Record<string, unknown>;
 }
