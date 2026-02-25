410f71489656d420a989d5679b42e4740e47344c
lifaon74 <valentinrich@gmail.com>
2026-02-25 14:07:23 +0100

    type: improve $extensions typing

--- packages/tokens/scripts/shared/dtcg/design-token/token/design-token.ts
+++ packages/tokens/scripts/shared/dtcg/design-token/token/design-token.ts
@@ -1,4 +1,3 @@
-import type { ExplicitAny } from '../../../../../../../scripts/helpers/types/explicit-any.ts';
 import type { ValueOrDesignTokenReference } from '../reference/value-or/value-or-design-token-reference.ts';
 
 /**
@@ -6,12 +5,14 @@ import type { ValueOrDesignTokenReference } from '../reference/value-or/value-or
  * @inheritDoc https://www.designtokens.org/tr/2025.10/format/#design-token-0
  *
  * > An object with a $value property is a token
-import type { ValueOrDesignTokenReference } from '../reference/value-or/value-or-design-token-reference.ts';
-
-/**
-@@ -14,5 +15,5 @@ export interface DesignToken<GType extends string, GValue> {
-readonly $type?: GType;
-readonly $deprecated?: boolean | string;
-readonly $description?: string;
-readonly $extensions?: Record<string, unknown>;
+ */
+export interface DesignToken<GType extends string, GValue> {
+  /**
+   * @alias $ref
+   */
+  readonly $value: ValueOrDesignTokenReference<GValue>;
+  readonly $type?: GType;
+  readonly $deprecated?: boolean | string;
+  readonly $description?: string;
+  readonly $extensions?: Record<string, unknown>;
 }
